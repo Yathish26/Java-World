58d532741144b7b0ec91cd7430b6618357976805
Yathish Acharya <yathishkumar2013@gmail.com>
2024-12-06 00:10:31 +0530

    hello Java

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        int a = 24;
+        int b = 12;
+        int result = MathUtils.add(a, b) + MathUtils.multiplication(a, b);
+        System.out.println(result);
+    }
+}
---
+++ MathUtils.java
@@ -0,0 +1,10 @@
+
+public class MathUtils {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+}
