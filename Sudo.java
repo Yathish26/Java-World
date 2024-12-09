285e7d20acc3a9bcfd554c9275f13384fccb232c
Yathish Acharya <yathishkumar2013@gmail.com>
2024-12-09 14:51:52 +0530

    ee

---
+++ Sudo.java
@@ -0,0 +1,13 @@
+public class Sudo {
+    public static void main(String[] args){
+       Hello();
+    }
+
+    public static void Hello(){
+        String Name = "Yathish";
+        int age = 24;
+        String place = "Mangalore";
+    
+        System.out.printf("Hello %s you are %d years old and you are from %s",Name,age,place);
+    }
+}
